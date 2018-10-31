09194ac51e298c59db4e92bef3ee9f6af16f0f7e
Christopher Bilger <3654150+ChristopherBilg@users.noreply.github.com>
2018-10-30 21:38:44 -0400

    Formatted to PEP8 guidelines

--- decay.py
+++ decay.py
@@ -6,136 +6,148 @@
 
 import numpy as np
 
+
 class GeomDecay:
-    """Schedule for geometrically decaying the simulated annealing temperature parameter T"""
-    
-    def __init__(self, init_t = 1.0, decay = 0.99, min_t = 0.001):
+    """
+    Schedule for geometrically decaying the simulated
+    annealing temperature parameter T
+    """
+
+    def __init__(self, init_t=1.0, decay=0.99, min_t=0.001):
         """Initialize decay schedule object
-        
-        Args: 
+
+        Args:
         init_t: float. Initial value of temperature parameter T
         decay: float. Temperature decay parameter.
         min_t: float. Minimum value of temperature parameter.
-        
+
         Returns:
         None
         """
         self.init_t = init_t
         self.decay = decay
         self.min_t = min_t
-        
-    def evaluate(self, t):
+
+    def evaluate(self, _t):
         """Evaluate the temperature parameter at time t
-        
-        Args:    
+
+        Args:
         t: int. Time at which the temperature paramter t is evaluated
-        
+
         Returns:
         temp: float. Temperature parameter at time t
         """
-        
-        temp = self.init_t*(self.decay**t)
-        
+
+        temp = self.init_t*(self.decay**_t)
+
         if temp < self.min_t:
             temp = self.min_t
-        
+
         return temp
+
 
 class ArithDecay:
-    """Schedule for arithmetically decaying the simulated annealing temperature parameter T"""
-    
-    def __init__(self, init_t = 1.0, decay = 0.0001, min_t = 0.001):
+    """
+    Schedule for arithmetically decaying the simulated
+    annealing temperature parameter T
+    """
+
+    def __init__(self, init_t=1.0, decay=0.0001, min_t=0.001):
         """Initialize decay schedule object
-        
-        Args: 
+
+        Args:
         init_t: float. Initial value of temperature parameter T
         decay: float. Temperature decay parameter.
         min_t: float. Minimum value of temperature parameter.
-        
+
         Returns:
         None
         """
         self.init_t = init_t
         self.decay = decay
         self.min_t = min_t
-        
-    def evaluate(self, t):
+
+    def evaluate(self, _t):
         """Evaluate the temperature parameter at time t
-        
-        Args:    
+
+        Args:
         t: int. Time at which the temperature paramter t is evaluated
-        
+
         Returns:
         temp: float. Temperature parameter at time t
         """
-        
-        temp = self.init_t - (self.decay*t)
-        
+
+        temp = self.init_t - (self.decay*_t)
+
         if temp < self.min_t:
             temp = self.min_t
-        
+
         return temp
 
+
 class ExpDecay:
-    """Schedule for exponentially decaying the simulated annealing temperature parameter T"""
-    
-    def __init__(self, init_t = 1.0, exp_const = 0.005, min_t = 0.001):
+    """
+    Schedule for exponentially decaying the simulated
+    annealing temperature parameter T
+    """
+
+    def __init__(self, init_t=1.0, exp_const=0.005, min_t=0.001):
         """Initialize decay schedule object
-        
-        Args: 
+
+        Args:
         init_t: float. Initial value of temperature parameter T
         exp_const: float. Exponential constant parameter.
         min_t: float. Minimum value of temperature parameter.
-        
+
         Returns:
         None
         """
         self.init_t = init_t
         self.exp_const = exp_const
         self.min_t = min_t
-        
-    def evaluate(self, t):
+
+    def evaluate(self, _t):
         """Evaluate the temperature parameter at time t
-        
-        Args:    
+
+        Args:
         t: int. Time at which the temperature paramter t is evaluated
-        
+
         Returns:
         temp: float. Temperature parameter at time t
         """
-        
-        temp = self.init_t*np.exp(-1.0*self.exp_const*t)
-        
+
+        temp = self.init_t*np.exp(-1.0*self.exp_const*_t)
+
         if temp < self.min_t:
             temp = self.min_t
-        
-        return temp   
+
+        return temp
+
 
 class CustomSchedule:
     """Class for generating your own temperature schedule."""
-    
+
     def __init__(self, schedule, **kwargs):
         """Initialize CustomSchedule object.
-        
+
         Args:
         schedule: function. Function for calculating the temperature at time t
         kwargs: dictionary. Additional arguments to be passed to schedule
-        
+
         Returns:
-        None       
+        None
         """
         self.schedule = schedule
         self.kwargs = kwargs
-    
-    def evaluate(self, t):
+
+    def evaluate(self, _t):
         """Evaluate the temperature parameter at time t
-        
-        Args:    
+
+        Args:
         t: int. Time at which the temperature paramter t is evaluated
-        
+
         Returns:
         temp: float. Temperature parameter at time t
         """
-        temp = self.schedule(t, **self.kwargs)
+        temp = self.schedule(_t, **self.kwargs)
         return temp
-        
--- test.py
+++ test.py
@@ -5,49 +5,55 @@
 """
 
 import unittest
-from fitness import *
-'''
-from algorithms import *
-from discrete import *
-from fitness import *
-from decay import *
-from neural import *
-import numpy as np
-'''
+from fitness import OneMax, FlipFlop, head, tail, max_run, Queens, np
+
+# from algorithms import *
+# from discrete import *
+# from fitness import *
+# from decay import *
+# from neural import *
+# import numpy as np
+
 
 class TestFitness(unittest.TestCase):
     """Tests for fitness.py."""
 
-    def test_onemax(self):
-        """Test OneMax fitness function"""        
+    @staticmethod
+    def test_onemax():
+        """Test OneMax fitness function"""
         state = np.array([0, 1, 0, 1, 1, 1, 1])
-        assert(OneMax().evaluate(state) == 5)
-        
-    def test_flipflop(self):
-        """Test FlipFlop fitness function"""        
+        assert OneMax().evaluate(state) == 5
+
+    @staticmethod
+    def test_flipflop():
+        """Test FlipFlop fitness function"""
         state = np.array([0, 1, 0, 1, 1, 1, 1])
-        assert(FlipFlop().evaluate(state) == 3)
-        
-    def test_head(self):
+        assert FlipFlop().evaluate(state) == 3
+
+    @staticmethod
+    def test_head():
         """Test head function"""
         state = np.array([1, 1, 1, 1, 0, 1, 0, 2, 1, 1, 1, 1, 1, 4, 6, 1, 1])
-        assert(head(1, state) == 4)
-    
-    def test_tail(self):
+        assert head(1, state) == 4
+
+    @staticmethod
+    def test_tail():
         """Test tail function"""
         state = np.array([1, 1, 1, 1, 0, 1, 0, 2, 1, 1, 1, 1, 1, 4, 6, 1, 1])
-        assert(tail(1, state) == 2)
-    
-    def test_max_run(self):
+        assert tail(1, state) == 2
+
+    @staticmethod
+    def test_max_run():
         """Test max_run function"""
         state = np.array([1, 1, 1, 1, 0, 1, 0, 2, 1, 1, 1, 1, 1, 4, 6, 1, 1])
-        assert(max_run(1, state) == 5)
-   
-    def test_queens(self):
+        assert max_run(1, state) == 5
+
+    @staticmethod
+    def test_queens():
         """Test Queens fitness function"""
         state = np.array([1, 4, 1, 3, 5, 5, 2, 7])
-        assert(Queens().evaluate(state) == 6)
-        
-    
+        assert Queens().evaluate(state) == 6
+
+
 if __name__ == '__main__':
     unittest.main()
