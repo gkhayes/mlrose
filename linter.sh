2447ec1db932ad4115347dec1958351b111fb0bb
Christopher R. Bilger <3654150+ChristopherBilg@users.noreply.github.com>
2018-11-01 11:35:02 -0400

    Extended --disable option

--- linter.sh
+++ linter.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 echo "Starting lint on algorithms.py"
-pylint algorithms.py --score=no --disable=R0902,R0903,R0904
+pylint algorithms.py --score=no --disable=R0902,R0903,R0904,R0201
 pycodestyle algorithms.py
 flake8 algorithms.py
 
 echo "Starting lint on decay.py"
-pylint decay.py --score=no --disable=R0902,R0903,R0904
+pylint decay.py --score=no --disable=R0902,R0903,R0904,R0201
 pycodestyle decay.py
 flake8 decay.py
 
 echo "Starting lint on discrete.py"
-pylint discrete.py --score=no --disable=R0902,R0903,R0904
+pylint discrete.py --score=no --disable=R0902,R0903,R0904,R0201
 pycodestyle discrete.py
 flake8 discrete.py
 
 echo "Starting lint on fitness.py"
-pylint fitness.py --score=no --disable=R0902,R0903,R0904
+pylint fitness.py --score=no --disable=R0902,R0903,R0904,R0201
 pycodestyle fitness.py
 flake8 fitness.py
 
 echo "Starting lint on test.py"
-pylint test.py --score=no --disable=R0902,R0903,R0904
+pylint test.py --score=no --disable=R0902,R0903,R0904,R0201
 pycodestyle test.py
 flake8 test.py
 
