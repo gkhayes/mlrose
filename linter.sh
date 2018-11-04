662ab6bf95c6a37afd546fad2ded17d45e4f3de1
Christopher R. Bilger <3654150+ChristopherBilg@users.noreply.github.com>
2018-11-04 18:31:14 -0500

    Added new ignore options

--- linter.sh
+++ linter.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 echo "Starting lint on algorithms.py"
-pylint algorithms.py --score=no --disable=R0902,R0903,R0904,R0201,R0913
+pylint algorithms.py --score=no --disable=R0201,R0902,R0903,R0904,R0913,R0914,C1801
 pycodestyle algorithms.py
 flake8 algorithms.py
 
 echo "Starting lint on decay.py"
-pylint decay.py --score=no --disable=R0902,R0903,R0904,R0201,R0913
+pylint decay.py --score=no --disable=R0201,R0902,R0903,R0904,R0913,R0914,C1801
 pycodestyle decay.py
 flake8 decay.py
 
 echo "Starting lint on discrete.py"
-pylint discrete.py --score=no --disable=R0902,R0903,R0904,R0201,R0913
+pylint discrete.py --score=no --disable=R0201,R0902,R0903,R0904,R0913,R0914,C1801
 pycodestyle discrete.py
 flake8 discrete.py
 
 echo "Starting lint on fitness.py"
-pylint fitness.py --score=no --disable=R0902,R0903,R0904,R0201,R0913
+pylint fitness.py --score=no --disable=R0201,R0902,R0903,R0904,R0913,R0914,C1801
 pycodestyle fitness.py
 flake8 fitness.py
 
 echo "Starting lint on test.py"
-pylint test.py --score=no --disable=R0902,R0903,R0904,R0201,R0913
+pylint test.py --score=no --disable=R0201,R0902,R0903,R0904,R0913,R0914,C1801
 pycodestyle test.py
 flake8 test.py
 
 echo "Starting lint on opt_probs.py"
-pylint opt_probs.py --score=no --disable=R0902,R0903,R0904,R0201,R0913
+pylint opt_probs.py --score=no --disable=R0201,R0902,R0903,R0904,R0913,R0914,C1801
 pycodestyle opt_probs.py
 flake8 opt_probs.py
 
