ef4b50b5544919eea54262ff3bea9d7c56581184
Genevieve Hayes <ghayes17@gmail.com>
2018-11-29 15:17:47 +1100

    Update project long_description in setup.py
    Set project long_description to be project README.md file.

--- setup.py
+++ setup.py
@@ -6,19 +6,16 @@
 
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+    
 setup(name = 'mlrose',
       version = '0.1.0',
       description = "MLROSe: Machine Learning, Randomized Optimization and"
                     + " Search",
-      long_description = "MLROSe provides classes and functions for solving"
-                    + " a range of well known AI/ML optimization problems," 
-                    + " including N-Queens, Travelling Salesperson and finding"
-                    + " the optimal weights for a neural network. The package"
-                    + " includes implementations of the hill climbing,"
-                    + " randomized hill climbing, simulated annealing,"
-                    + " genetic algorithm and MIMIC algorithms, and includes"
-                    + " functionality for both pre-defined and user-defined"
-                    + " fitness functions.",
+      long_description = readme(),
+      long_description_content_type='text/markdown',
       url = 'https://github.com/gkhayes/mlrose',
       author = 'Genevieve Hayes',
       license = 'BSD',
