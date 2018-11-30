d814dbbcc775968835f569d2e9b4b614972889bf
Christopher R. Bilger <3654150+ChristopherBilg@users.noreply.github.com>
2018-11-30 11:11:32 -0500

    Updated linter

--- linter.sh
+++ linter.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 disable_options="R0201,R0902,R0903,R0904,R0912,R0913,R0914,R0915,C0103,C0200,C1801,W0612"
-python_files=("algorithms.py"
-              "test_algorithms.py"
-              "decay.py"
-              "test_decay.py"
-              "fitness.py"
-              "test_fitness.py"
-              "activation.py"
-              "test_activation.py"
-              "neural.py"
-              "test_neural.py"
-              "opt_probs.py"
-              "test_opt_probs.py")
+python_files=("mlrose/algorithms.py"
+              "tests/test_algorithms.py"
+              "mlrose/decay.py"
+              "tests/test_decay.py"
+              "mlrose/fitness.py"
+              "tests/test_fitness.py"
+              "mlrose/activation.py"
+              "tests/test_activation.py"
+              "mlrose/neural.py"
+              "tests/test_neural.py"
+              "mlrose/opt_probs.py"
+              "tests/test_opt_probs.py")
 
 for filename in "${python_files[@]}"
 do
