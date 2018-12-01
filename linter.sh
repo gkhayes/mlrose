cd5f3abbb99ad5edaa508b4178c53d59440785f2
Christopher R. Bilger <3654150+ChristopherBilg@users.noreply.github.com>
2018-12-01 11:58:08 -0500

    Added ignore option for module line length > 1000

--- linter.sh
+++ linter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-disable_options="R0201,R0902,R0903,R0904,R0912,R0913,R0914,R0915,C0103,C0200,C1801,W0612"
+disable_options="R0201,R0902,R0903,R0904,R0912,R0913,R0914,R0915,C0103,C0200,C0302,C1801,W0612"
 python_files=("mlrose/algorithms.py"
               "tests/test_algorithms.py"
               "mlrose/decay.py"
