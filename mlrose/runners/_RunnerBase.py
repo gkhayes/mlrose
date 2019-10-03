cece255ab2fc393fdae39f698f86fb89c7a69551
Andrew Rollings <a.rollings@hiive.com>
2019-10-03 03:22:27 -0400

    WARNING: Refactored Neural Stuff. You may have to fix imports.
    (curve time interpolation in base runner)

--- mlrose/runners/_RunnerBase.py
+++ mlrose/runners/_RunnerBase.py
@@ -110,10 +110,10 @@ class _RunnerBase(ABC):
         df.to_csv(f'{filename_root}.csv')
 
     @staticmethod
-    def _create_curve_stat(iteration, time_s, fitness, curve_data):
+    def _create_curve_stat(iteration, fitness, curve_data):
         curve_stat = {
             'Iteration': iteration,
-            'Time': time_s,
+            'Time': None,
             'Fitness': fitness
         }
         curve_stat.update(curve_data)
@@ -162,6 +162,9 @@ class _RunnerBase(ABC):
             fc = list([(0, self._initial_fitness)]) + list(zip(range(1, iteration + 1),
                                                                [f for f in curve]))  # [1.0 / f for f in curve]))
 
-            curve_stats = [self._create_curve_stat(i, t, v, all_stats) for (i, v) in fc]
+            curve_stats = [self._create_curve_stat(i, v, all_stats) for (i, v) in fc]
+
+            for i in range(len(curve_stats)):
+                curve_stats[i]['Time'] = (t * i) / len(curve_stats)
             self._fitness_curves.extend(curve_stats)
         return not done
