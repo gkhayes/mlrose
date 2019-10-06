d85022d5268a8f5163affd21dbddaacf24332e83
Andrew Rollings <a.rollings@hiive.com>
2019-10-06 09:59:42 -0400

    updated gd export

--- mlrose/algorithms/__init__.py
+++ mlrose/algorithms/__init__.py
@@ -7,6 +7,7 @@ from .ga import (genetic_alg)
 from .sa import (simulated_annealing)
 from .hc import (hill_climb)
 from .rhc import (random_hill_climb)
+from .gd import (gradient_descent)
 from .mimic import (mimic)
 
 from .crossovers import UniformCrossOver, TSPCrossOver, OnePointCrossOver
