c0f0935342073ed27a5365b69a210e4e7a71d156
Andrew Rollings <a.rollings@hiive.com>
2019-10-06 20:30:49 -0400

    Sorry guys. I renamed all of the files to be more Pythonic. This means you'll likely have to change all of your inputs.
    Minor formatting issues.

--- mlrose/neural/utils/__init__.py
+++ mlrose/neural/utils/__init__.py
@@ -1,1 +1,6 @@
+""" Classes for defining neural network weight optimization problems."""
+
+# Author: Genevieve Hayes (modified by Andrew Rollings)
+# License: BSD 3 clause
+
 from .weights import (flatten_weights, unflatten_weights, gradient_descent_original)
