60503bcfd9ad8eacc989fb615ecb4879f74a97f4
Andrew Rollings <a.rollings@hiive.com>
2019-10-11 11:48:56 -0400

    Make GridSearchMixin score method more robust. Thanks to Erika Gemzer.

--- mlrose/gridsearch/grid_search_mixin.py
+++ mlrose/gridsearch/grid_search_mixin.py
@@ -7,6 +7,7 @@ class GridSearchMixin:
     def __init__(self, scorer_method=None):
         self._scorer_method = skmt.balanced_accuracy_score if scorer_method is None else scorer_method
         self._params = inspect.signature(self._scorer_method)
+        self._get_y_argmax = False
 
     def _perform_grid_search(self, classifier, x_train, y_train, cv, parameters, n_jobs=1, verbose=False):
         scorer = self.make_scorer()
@@ -30,5 +31,15 @@ class GridSearchMixin:
 
     def _grid_search_score_intercept(self, y_pred, y_true, **kwargs):
         cleaned_kwargs = {k: v for k, v in kwargs.items() if k in list(inspect.signature(self._scorer_method).parameters)}
+
+        if not self._get_y_argmax and len(y_pred.shape) > 1 and len(y_true.shape) > 1:
+            try:
+                return self._scorer_method(y_pred=y_pred, y_true=y_true, **cleaned_kwargs)
+            except:
+                self._get_y_argmax = True
+
+        if self._get_y_argmax:
+            y_pred = y_pred.argmax(axis=1)
+            y_true = y_true.argmax(axis=1)
 
         return self._scorer_method(y_pred=y_pred, y_true=y_true, **cleaned_kwargs)
