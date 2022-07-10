2a3d469eb247b850650d85087979dd87c48ed246
Georg Wendorf <22711712+pcjedi@users.noreply.github.com>
2022-07-10 21:58:47 +0200

    Update neural.py

--- mlrose/neural.py
+++ mlrose/neural.py
@@ -9,7 +9,7 @@ from abc import abstractmethod
 import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin, RegressorMixin
 from sklearn.metrics import mean_squared_error, log_loss
-from sklearn.externals import six
+import six
 from .activation import identity, relu, sigmoid, softmax, tanh
 from .algorithms import random_hill_climb, simulated_annealing, genetic_alg
 from .opt_probs import ContinuousOpt
